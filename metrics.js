c15b8cd9919668b47de4681ff4c57d579dd6f49a
dileepscl <dileepscl@gmail.com>
2026-03-01 13:53:45 +0530

    Add executive metrics data layer

---
+++ metrics.js
@@ -0,0 +1,11 @@
+const EXECUTIVE_METRICS = {
+  totalImpact: 10200000,
+  directSavings: 3000000,
+  operationalImpact: 7200000,
+  portfolioExposure: 40000000,
+  avgPerformanceImprovement: 46.5,
+  companiesTransformed: 3,
+  governanceStrength: 9,
+  complexityIndex: 8,
+  leverageRatio: 25.5
+};
